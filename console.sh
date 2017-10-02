878f42595dcccc796e330f5bc2644e22c9f4538f
Septiyadi <septiyadi@mhs.pelitabangsa.ac.id>
2017-10-02 17:07:13 +0700

    Create console.sh

---
+++ console.sh
@@ -0,0 +1,14 @@
+## Install Git
+moeslim@developer:~$ sudo apt -y install git
+
+## Install Java JDK (Java Development Kit)
+moeslim@developer:~$ sudo apt -y install default-jdk
+
+## Install Wizard Persiapan Android Studio memandu Anda melalui persiapan selanjutnya, termasuk mengunduh komponen Android SDK yang diperlukan untuk pengembangan.
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+
+sumber: https://linuxconfig.org/how-to-install-android-studio-on-ubuntu-16-04-xenial-xerus-linux
+
+## Download Android Studio Package on Website
+https://developer.android.com/studio/index.html
+
